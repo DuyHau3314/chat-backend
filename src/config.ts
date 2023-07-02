3f0af014c6c47ed16809717bfd0b7e6f3e8b6059
Henry Trinh <henry>
2023-07-02 16:18:10 +0700

    feat: implementd auth feature

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import bunyan from 'bunyan';
+import cloudinary from 'cloudinary';
 
 dotenv.config();
 
@@ -12,6 +13,9 @@ class Config {
   public CLIENT_URL = '';
   public NODE_ENV = '';
   public REDIS_HOST = '';
+  public CLOUD_NAME = '';
+  public CLOUD_API_KEY = '';
+  public CLOUD_API_SECRET = '';
 
   private readonly DEFAULT_DATABASE_URL = 'mongodb://localhost:27017/chattyapp-backend';
 
@@ -24,6 +28,9 @@ class Config {
     this.CLIENT_URL = process.env.CLIENT_URL || '';
     this.NODE_ENV = process.env.NODE_ENV || '';
     this.REDIS_HOST = process.env.REDIS_HOST || '';
+    this.CLOUD_NAME = process.env.CLOUD_NAME || '';
+    this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
+    this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
   }
 
   public createLogger(name: string): bunyan {
@@ -36,6 +43,14 @@ class Config {
         throw new Error(`Missing config value for ${key}`);
       }
     }
+  }
+
+  public cloudinaryConfig(): void {
+    cloudinary.v2.config({
+      cloud_name: this.CLOUD_NAME,
+      api_key: this.CLOUD_API_KEY,
+      api_secret: this.CLOUD_API_SECRET
+    });
   }
 }
 
